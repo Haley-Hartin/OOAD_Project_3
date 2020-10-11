f936b60ecd22c8e8fbfce79f5ebf37c19f76b2b3
Haley <hmdrexel@gmail.com>
2020-10-10 21:28:31 -0600

    Updated Casual Ordering style so it only orders up to 3 rolls, not 4

--- CasualCustomer.java
+++ CasualCustomer.java
@@ -10,7 +10,7 @@ public class CasualCustomer extends Customer{
 
 	public void decideOrder() {
 		Random rand = new Random(); ////https://www.geeksforgeeks.org/java-util-random-nextint-java/)
-		int numRolls = rand.nextInt(4) + 1; //1-3 rolls
+		int numRolls = rand.nextInt(3) + 1; //1-3 rolls
 		List<String> order = new ArrayList<String>(); 
 		for(int i = 0; i < numRolls; i++) { ////https://www.baeldung.com/java-random-list-element
 			String type = getMenu().get(rand.nextInt(getMenu().size()));
