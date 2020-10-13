02edf89d53a77d181886705e42e602ea20f26ffc
Haley-Hartin <55203391+Haley-Hartin@users.noreply.github.com>
2020-10-13 15:22:43 -0600

    added comments

--- Inventory.java
+++ Inventory.java
@@ -12,16 +12,16 @@ public class Inventory {
 		//https://stackoverflow.com/questions/6589744/how-to-return-a-list-of-keys-from-a-hash-map
 		
 		for(int i = 0; i < keys.size(); i++) {
-			this.inventory.put(keys.get(i), inventorySize);
+			this.inventory.put(keys.get(i), inventorySize); //set each inventory item to 30 (inventorySize)
 		}
 	}
 	
-	
+	//if an inventory item is at 0, set it to 30
 	public void restockInventory() { //https://beginnersbook.com/2013/12/hashmap-in-java-with-example/
 		String restockMessage = "The inventory was restocked with";
 		int restockedInventory = 0;
 		Iterator<Map.Entry<String, Integer>> iterator = this.inventory.entrySet().iterator();
-	    while(iterator.hasNext()) {
+	    while(iterator.hasNext()) { //loop through inventory items and set items with 0 inventory to 30
 	       Map.Entry<String, Integer> mentry = iterator.next(); 
 	       if(mentry.getValue() == 0) {
 	    	   this.inventory.put(mentry.getKey(), this.inventorySize);
@@ -41,6 +41,7 @@ public class Inventory {
 	
 	public int checkInventoryForEmptiedStock() { 	    
 	    List<Integer> inventoryValues = new ArrayList<Integer>(this.inventory.values()); 
+	    //check if every item is at 0 inventory 
 	    int zeroedInventory = Collections.frequency(inventoryValues, 0);//https://www.geeksforgeeks.org/java-util-collections-frequency-java-examples/
 
 	    return zeroedInventory;
@@ -48,6 +49,7 @@ public class Inventory {
 	
 	public void announceInventory() {
     	String inventoryReport = "The inventory has: ";
+        //loop through inventory and print out contents 
     	for(int j = 0; j < this.inventory.size(); j++) {
 			String key = new ArrayList<>(this.inventory.keySet()).get(j);
 			inventoryReport = inventoryReport +" " + this.inventory.get(key) + " " + key + "(s)," ;
@@ -65,10 +67,12 @@ public class Inventory {
 		}
 	}
 	
+	//reduce stock when item is ordered
 	public void reduceStock(String key) {
 		this.inventory.put(key, this.inventory.get(key) - 1);
 	}
 	
+	//increase stock incase order cant be fufilled 
 	public void increaseStock(String key) {
 		this.inventory.put(key, this.inventory.get(key) + 1);
 	}
